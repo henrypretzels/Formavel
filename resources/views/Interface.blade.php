70437001b37288e6222dd574e1f21c8ca41dcf51
Henrique Prestes <coffeekitestudios@gmail.com>
2025-05-01 18:19:36 -0300

    feat: commit inicial com a base do projeto

---
+++ resources/views/Interface.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Teste de Interface</title>
+</head>
+<body>
+    <header>
+        <h1>Hello World!</h1>
+    </header>
+</body>
+</html>
