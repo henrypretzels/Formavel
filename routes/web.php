3e160b2398c3789894300f722a2f5759e15df6fb
GuilhermeWeimer <guilhermeweimer@hotmail.com>
2025-05-05 11:09:15 -0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,23 @@
 <?php
+//para o primeiro site teste
+
 use Illuminate\Support\Facades\Route;
 Route::view('/', 'home');
+
+//Para o site do petshop
+
+//use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
+Route::get('/petshop', function () {
+    return view('petshop');
+})->name('petshop.view');
+
+// Simulação de POST — ainda sem lógica
+Route::post('/petshop', function (Request $request) {
+    // Apenas exemplo — salve no banco ou envie e-mail
+    return back()->with('success', 'Formulário enviado!');
+})->name('petshop.store');
 
 
 ?>
